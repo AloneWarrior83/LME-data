ce55e5ae3be3cea55a44d6e59858409ab21c947d
AloneWarrior83 <AhmadSajjadBadakhshi.af@gmail.com>
2026-05-31 18:27:19 +0430

    update v1

--- push.bat
+++ push.bat
@@ -1,5 +1,19 @@
 @echo off
 cd "C:\Users\HAISB STORE\LME-data"
+
+:: خوندن عدد فعلی
+set /p version=<version.txt
+
+:: یه واحد زیاد کردن
+set /a version=%version%+1
+
+:: نوشتن عدد جدید
+echo %version%>version.txt
+
+:: push به GitHub
 git add .
-git commit -m "auto update %date% %time%"
+git commit -m "update v%version%"
 git push
+
+echo Done! Version: %version%
+pause
