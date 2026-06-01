9114da21b00fe6c1cd202c823f55ed3cc694cf26
AloneWarrior83 <AhmadSajjadBadakhshi.af@gmail.com>
2026-06-01 08:54:57 +0430

    update v1-1-1-1

--- push.bat
+++ push.bat
@@ -1,10 +1,27 @@
 @echo off
 cd "C:\Users\HAISB STORE\LME-data"
 
-set /p version=<version.txt
-set /a version=%version%+1
-(echo %version%)>version.txt
+:: خوندن version های فعلی
+for /f "tokens=1,2 delims==" %%a in (version.txt) do set %%a=%%b
+
+:: چک کردن کدوم فایل تغییر کرده
+git diff --name-only > changed_files.txt
+
+findstr /i "LME_database.xlsx" changed_files.txt >nul && set /a LME_database=%LME_database%+1
+findstr /i "WagonsWithWeight.xlsx" changed_files.txt >nul && set /a WagonsWithWeight=%WagonsWithWeight%+1
+findstr /i "master_tabel_for_tracks.xlsx" changed_files.txt >nul && set /a trucks=%trucks%+1
+findstr /i "loaded.xlsx" changed_files.txt >nul && set /a waiting=%waiting%+1
+
+del changed_files.txt
+
+:: نوشتن version های جدید
+(
+echo LME_database=%LME_database%
+echo WagonsWithWeight=%WagonsWithWeight%
+echo trucks=%trucks%
+echo waiting=%waiting%
+)>version.txt
 
 git add .
-git commit -m "update v%version%"
+git commit -m "update v%LME_database%-%WagonsWithWeight%-%trucks%-%waiting%"
 git push
