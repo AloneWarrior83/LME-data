7a7eb32c1508331706e7c831ac2734724923e757
AloneWarrior83 <AhmadSajjadBadakhshi.af@gmail.com>
2026-06-07 20:58:57 +0430

    update v13-14-12-14-

--- push.bat
+++ push.bat
@@ -11,6 +11,7 @@ findstr /i "LME_database.json" changed_files.txt >nul && set /a LME_database=%LM
 findstr /i "WagonsWithWeight.json" changed_files.txt >nul && set /a WagonsWithWeight=%WagonsWithWeight%+1
 findstr /i "master_tabel_for_tracks.json" changed_files.txt >nul && set /a trucks=%trucks%+1
 findstr /i "loaded.json" changed_files.txt >nul && set /a waiting=%waiting%+1
+findstr /i "ForLoading.json" changed_files.txt >nul && set /a forLoading=%forLoading%+1
 
 del changed_files.txt
 
@@ -20,10 +21,11 @@ echo LME_database=%LME_database%
 echo WagonsWithWeight=%WagonsWithWeight%
 echo trucks=%trucks%
 echo waiting=%waiting%
+echo forLoading=%forLoading%
 )>version.txt
 
 git add .
-git commit -m "update v%LME_database%-%WagonsWithWeight%-%trucks%-%waiting%"
+git commit -m "update v%LME_database%-%WagonsWithWeight%-%trucks%-%waiting%-%forLoading%"
 git push
 
-echo Done! Versions: LME=%LME_database% Wagons=%WagonsWithWeight% Trucks=%trucks% Waiting=%waiting%
+echo Done! Versions: LME=%LME_database% Wagons=%WagonsWithWeight% Trucks=%trucks% Waiting=%waiting% ForLoading=%forLoading%
